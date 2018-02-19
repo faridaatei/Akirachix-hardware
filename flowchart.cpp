7ff02a265f3797359827faa723b4c9a952f5a8be
Farida Atei <faridaabdalla0@gmail.com>
2018-02-19 16:19:00 +0300

    added a repo

---
+++ flowchart.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, HTML, CSS, JS.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+ int a;
+ int b;
+
+cout<<"enter first number";
+
+cin>>a;
+
+cout<<"enter second number";
+
+cin>>b;
+
+if(a>b){
+ cout<<"first number is greater than second number";
+    
+}
+
+else if(a==b){
+    cout<<"first number is equals to second number";
+} 
+
+else{
+    cout<<"second number is greater than first number";
+
+
+
+    return 0;
+}
